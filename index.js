993ecfb60f2ff5d63f6a4ee676a367a899dc44d9
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-05-07 23:40:46 -0400

    added assigment instructions to index.js

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
-// cd Documents/Viking/JS/githuh
+/*
+ cd Documents/Viking/JS/githuh
+
+ CLI should support these commands:
+   1. githuh repos <username> should return a list of the user's recent repos.
+
+   2. githuh stars <username> should return a list of the user's starred repos.
+
+   3. githuh profile <username> should return the user's profile information
+   (email, number of public repos, follower/following count, etc.)
+
+ Github api documentation  ->  https://developer.github.com/v3/
+*/
